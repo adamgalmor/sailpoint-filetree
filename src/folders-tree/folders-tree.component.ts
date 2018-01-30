ed775521c84f293cf95ff42f326c4422ba0c3acf
Adam <adamgalmor@gmail.com>
2018-01-30 13:47:19 +0200

    TreeNode

--- src/folders-tree/folders-tree.component.ts
+++ src/folders-tree/folders-tree.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FoldersTreeService } from './shared/folders-tree.service'
 import { Observable } from 'rxjs/Observable';
 import { Folder } from '../shared/models/folder';
+import { ITreeOptions, TreeNode} from 'angular-tree-component';
+
 @Component({
   selector: 'app-folders-tree',
   templateUrl: './folders-tree.component.html',
@@ -10,32 +12,9 @@ import { Folder } from '../shared/models/folder';
 export class FoldersTreeComponent implements OnInit {
 
   public folders: Folder[];
-
-  nodes = [
-    {
-      id: 1,
-      name: 'root1',
-      children: [
-        { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'root2',
-      children: [
-        { id: 5, name: 'child2.1' },
-        {
-          id: 6,
-          name: 'child2.2',
-          children: [
-            { id: 7, name: 'subsub' }
-          ]
-        }
-      ]
-    }
-  ];
-  options = {};
+  public options: ITreeOptions = {
+    getChildren: this.getChildren.bind(this)
+  };
   
   constructor(private foldersTreeService: FoldersTreeService) { }
 
@@ -46,4 +25,10 @@ export class FoldersTreeComponent implements OnInit {
     })
   }
 
+  getChildren(node: any) {
+    return this.foldersTreeService.getFolders(node.id).toPromise();    
+  }
+  
+  onActivate = ($event) => console.log($event);
 }
+
